6e41e8f35491476e566e74f6b5b3cb24b6d3103b
Topher Daisy <cwd0032@auburn.edu>
2023-12-01 19:26:38 -0500

    final changes v2

--- KubeSec-master/logging_helper.py
+++ KubeSec-master/logging_helper.py
@@ -4,7 +4,7 @@ def create_helper():
     fileName  = '2023-11-09.log' 
     formatStr = '%(asctime)s %(message)s'
     logging.basicConfig(format=formatStr, filename=fileName, level=logging.INFO)
-    return logging.getLogger('team-24-logger')
+    return logging.getLogger('team-TPS-logger')
 
 def info(logged_string):
     create_helper().info(logged_string)
